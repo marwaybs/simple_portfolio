acde28505bebc86639ee7996480ec064a38c2209
marwaybs <bsmarway@gmail.com>
2019-10-08 20:32:28 -0400

    stlyed project boxes and added hover image changed

--- src/Containers/Footer/index.js
+++ src/Containers/Footer/index.js
@@ -1,31 +1,27 @@
 import React from 'react';
 import styled from 'styled-components'
-import github_logo from '../../Assets/Images/github_logo.png'
+// import github_logo from '../../Assets/Images/github_logo.png'
 
 const FooterWrapper  = styled.div`
     
 `
 
-const Title = styled.p`
-  margin: 0px;
-  font-size: 40px;
+const Text = styled.p``;
+
+const Link = styled.a`
+  
 `
 
-const SubTitle = styled.p`
-  font-size: 30px;
-`
-
-const GithubLogo = styled.img`
-  max-width: 100px;
-`;
-
-const TechnologyList = styled.p``;
+// const GithubLogo = styled.img`
+//   max-width: 100px;
+// `;
 
 export default function Intro() {
   return (
       <FooterWrapper>
-        <p type="email">bsmarway@gmail.com</p>
-        <GithubLogo src={github_logo}></GithubLogo>
+        <Text>Email:</Text>
+        <Link href="mailto:bsmarway@gmail.com" target="_top">bsmarway@gmail.com</Link>
+        
       </FooterWrapper>
   );
 }
--- src/Containers/Projects/Projects.js
+++ src/Containers/Projects/Projects.js
@@ -2,39 +2,69 @@ import React from 'react';
 import { dark } from '../../Components/colors';
 import styled from 'styled-components'
 import { projectData } from './data'
+import portfolio from './../../Assets/Images/portfolio.png'
 
 
 const Grid = styled.div`
   display: flex;
 `;
 
+const InfoWrapper = styled.div`
+  display: none;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: auto;
+`;
+
+
 const ProjectWrapper = styled.div`
   text-align: left;
   border: 2px solid ${dark};
-  max-width: 500px
+  // max-width: 500px;
+  // max-height: 500px;
+  height: 200px;
+  width: 200px;
   flex: 1;
+  margin: 5px;
+  :hover {
+    cursor: pointer;
+    ${InfoWrapper} {
+      display: unset;
+    }
+    ${Image} {
+      display: none;
+    }
+  }
 `
 
 const Subtitle = styled.p`
-  font-size: 30px;
+  margin: 0;
+  font-size: 20px;
 `;
 
 const Description = styled.p`
-  font-size: 15px;
+  font-size: 12px;
 `
 
 const Technologies = styled.p`
-  font-size: 15px;
+  font-size: 10px;
+  color: grey;
 `
 
 export default function Projects() {
-    const projects = projectData.map((project) => 
-    <ProjectWrapper>
-        <Subtitle>{project.subtitle}</Subtitle>
-        <Description>{project.description}</Description>
-        <Technologies>{project.technologies}</Technologies>
-    </ProjectWrapper>
-    )
+  console.log(projectData);
+  const projects = projectData.map((project) => 
+  <ProjectWrapper>
+    <InfoWrapper>
+      <Subtitle>{project.subtitle}</Subtitle>
+      <Description>{project.description}</Description>
+      <Technologies>{project.technologies}</Technologies>
+    </InfoWrapper>
+    <Image src={project.image} />
+  </ProjectWrapper>
+  )
   return (
       <Grid>
         {projects}
