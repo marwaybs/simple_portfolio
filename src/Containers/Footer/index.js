1c0f19a9746ea7d040939aab20fc8f65456badaa
marwaybs <bsmarway@gmail.com>
2019-10-10 10:15:07 -0400

    styling footer

--- src/Containers/Footer/index.js
+++ src/Containers/Footer/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import styled from 'styled-components'
+import { dark, light } from '../../Components/colors';
 // import github_logo from '../../Assets/Images/github_logo.png'
 
 const FooterWrapper  = styled.div`
-    
+    background-color: ${dark}
 `
 
-const Text = styled.p``;
+const Text = styled.p`
+  color: ${light}
+`;
 
 const Link = styled.a`
-  
+  color: ${light};
 `
 
 // const GithubLogo = styled.img`
@@ -21,7 +24,6 @@ export default function Intro() {
       <FooterWrapper>
         <Text>Email:</Text>
         <Link href="mailto:bsmarway@gmail.com" target="_top">bsmarway@gmail.com</Link>
-        
       </FooterWrapper>
   );
 }
