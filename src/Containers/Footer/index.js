305ceb96ec7bc1befb939e121fdedf6e1a57b0b4
marwaybs <bsmarway@gmail.com>
2019-10-10 11:10:06 -0400

    more footer styling, fixing flex wrap

--- src/Containers/Footer/index.js
+++ src/Containers/Footer/index.js
@@ -5,25 +5,47 @@ import { dark, light } from '../../Components/colors';
 
 const FooterWrapper  = styled.div`
     background-color: ${dark}
+    margin-top: 20px;
+    display: flex;
 `
+const Section = styled.div`
+  padding: 10px;
+  margin: auto;
+  text-align: center;
+  min-width: 100px;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
 
-const Text = styled.p`
+const Text = styled.span`
   color: ${light}
 `;
 
+const Title = styled.p`
+  color: ${light};
+`;
+
+
 const Link = styled.a`
   color: ${light};
+  text-decoration: none;
 `
-
-// const GithubLogo = styled.img`
-//   max-width: 100px;
-// `;
 
 export default function Intro() {
   return (
       <FooterWrapper>
-        <Text>Email:</Text>
-        <Link href="mailto:bsmarway@gmail.com" target="_top">bsmarway@gmail.com</Link>
+        <Section>
+          <Title>Contact me:</Title>
+        </Section>
+        <Section>
+          <Link href="mailto:bsmarway@gmail.com" target="_top">bsmarway@gmail.com</Link>
+        </Section>
+        <Section>
+          <Link href="https://github.com/marwaybs" target="_blank">Github</Link>
+        </Section>
+        <Section>
+          <Link href="https://ca.linkedin.com/in/bhaag-marway-5630b0a8" target="_blank">Linkedin</Link>
+        </Section>
       </FooterWrapper>
   );
 }
