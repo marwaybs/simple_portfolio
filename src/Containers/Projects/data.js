168c9a750652b28c6753b0909ea247276efd974d
marwaybs <bsmarway@gmail.com>
2019-10-11 13:53:02 -0400

    added unique images for projects

--- src/Containers/Projects/data.js
+++ src/Containers/Projects/data.js
@@ -1,4 +1,8 @@
-import portfolio from './../../Assets/Images/portfolio.png'
+import portfolio from './../../Assets/Images/portfolio.png';
+import contact from './../../Assets/Images/contact.svg';
+import music_note from './../../Assets/Images/music_note.png';
+import tic_tac_toe from './../../Assets/Images/tic_tac_toe.png';
+import guided_daydreaming from './../../Assets/Images/guided_daydreaming.svg';
 
 export const projectData = [
     {
@@ -12,28 +16,28 @@ export const projectData = [
         subtitle: 'Contacts App',
         description: 'Exploring the new technologies used with react development as reccomended by react boilerplate',
         technologies: 'React, Redux, Redux Saga, styled components, Ruby on Rails',
-        image: portfolio,
+        image: contact,
         link: 'https://github.com/marwaybs/simple_portfolio'
     },
     {
         subtitle: 'Tic Tac Toe Game',
         description: 'Wanted to build a simple app to explore vanilla js a bit. Future addition is to add a cpu, animations',
         technologies: 'React, vanilla JS',
-        image: portfolio,
+        image: tic_tac_toe,
         link: 'https://github.com/marwaybs/tic_tac_toe'
     },
     {
         subtitle: 'Musicality Trainer',
         description: 'A cross platform mobile app to train musicality for dancing.',
         technologies: 'React Native',
-        image: portfolio,
+        image: music_note,
         link: 'https://github.com/marwaybs/musicality'
     },
     {
         subtitle: 'Guided Daydreaming',
         description: 'An experimental computational therapy based on EMDR',
         technologies: 'Rails, JS, bulma.css',
-        image: portfolio,
+        image: guided_daydreaming,
         link: 'https://github.com/marwaybs/guidedDaydreaming'
     }
 ]
